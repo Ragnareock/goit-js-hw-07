f542a9b8e5a96c5efb1be4753da81c4979fa91a8
Andriy Lobash <110089586+Ragnareock@users.noreply.github.com>
2023-05-09 01:56:22 +0300

    Update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+
+function createImageCardsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+        `;
+    })
+    .join('');
+}
+
+galleryListEl.innerHTML = createImageCardsMarkup(galleryItems);
+
+galleryListEl.addEventListener('click', onImageContainerClick);
+
+function onImageContainerClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+    console.log(evt.target);
+}
